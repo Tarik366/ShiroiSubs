78ba87e6db44ff3fba2410e2ef1710eda935b7cd
Tarikgamer <tarik.gamers@outlook.com>
2021-11-14 23:30:54 +0300

    koyu tema sona yakı

--- ShiroiSubs/app/static/Style.js
+++ ShiroiSubs/app/static/Style.js
@@ -1,3 +1,20 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#222',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: 'light_mode', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
 function addDarkmodeWidget() {
     new Darkmode().showWidget();
 }
